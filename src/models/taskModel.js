7dece4aab3d5019e7fdb05ee8bff1c96d60f9f21
toscy <123191534+Lajous-Harold@users.noreply.github.com>
2025-10-15 16:58:19 +0200

    Added comment in taskModel.js

--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -1,4 +1,9 @@
+/* Task Model 
+    A task is defined by a title and an optional description.
+*/
+
 class Task {
+  // Initialize a new Task with title and a description (by default, an empty string)
   constructor(title, description = "") {
     this.title = title;
     this.description = description;
